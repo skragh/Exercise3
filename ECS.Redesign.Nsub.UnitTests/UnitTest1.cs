99779489b1fb62c483876c6efcde5d091fcea8c0
Sigurd Skov Jensen <Sigurd.skov@hotmail.com>
2021-09-23 11:07:53 +0200

    Gennemlæst, lavet kommentar

--- ECS.Redesign.Nsub.UnitTests/UnitTest1.cs
+++ ECS.Redesign.Nsub.UnitTests/UnitTest1.cs
@@ -11,18 +11,23 @@ namespace ECS.Redesign.Nsub.UnitTests
         [SetUp]
         public void Setup()
         {
+            //Skab substitut
             fakeHeater = Substitute.For<IHeater>();
             fakeSensor = Substitute.For<ITempSensor>();
+            //Opsæt Unit under test
             uut = new ECS(25, fakeSensor, fakeHeater);
         }
 
         [TestCase(0, 20)]
         [TestCase(25, 30)]
-        public void ECSRegulate_TempAboveTreshhold_AssertHeaterOn(int threshold, int tempSensorTemp)
+        public void ECSRegulate_TempAboveTreshhold_AssertHeaterOff(int threshold, int tempSensorTemp)
         {
+            //arrange
             fakeSensor.GetTemp().Returns(tempSensorTemp);
             uut.SetThreshold(threshold);
+            //Act
             uut.Regulate();
+            //Assert
             fakeHeater.Received().TurnOff();
         }
 
@@ -30,9 +35,12 @@ namespace ECS.Redesign.Nsub.UnitTests
         [TestCase(30,25)]
         public void ECSRegulate_TempBelowTreshhold_AssertHeaterOn(int threshold, int tempSensorTemp)
         {
+            //Arrange
             fakeSensor.GetTemp().Returns(tempSensorTemp);
             uut.SetThreshold(threshold);
+            //Act
             uut.Regulate();
+            //Assert
             fakeHeater.Received().TurnOn();
         }
     }
